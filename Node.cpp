57388a6ebff98a133eb0c855fc426fc06e511f3d
aedelgado19 <aedelgado19@gmail.com>
2020-11-01 16:02:19 -0800

    node classes!

---
+++ Node.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "node.h"
+using namespace std;
+
+//get next node                                                                                          
+Node* Node::getNext(){
+  return next;
+
+}
+
+//set next node                                                                                          
+void Node::setNext(Node* node){
+  next = node;
+
+}
+
+void Node::setStudent(Student *inputStudent){
+  studentPtr = inputStudent;
+
+}
+
+Student* Node::getStudent(){
+  return studentPtr;
+}
+
+
+//constructor                                                                                            
+Node::Node(Student* inputStudent){
+  studentPtr = inputStudent;
+
+}
+
+//destructor                                                                                             
+Node::~Node(){
+  delete studentPtr;
+}
---
+++ Node.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "student.h"
+//header for node class                                                                                  
+class Node{
+public:
+  Node* getNext();
+  void setNext(Node* node);
+  Node(Student* inputStudent);
+  ~Node();
+  void setStudent(Student *inputStudent);
+  Student* getStudent();
+private:
+  Student *studentPtr;
+  Node* next;
+};
+
+
